2aa85e506cf46a93fd786a6e9f1280c2babc4912
Lucas <lucashtw@gmail.com>
2022-03-10 11:21:03 -0300

    JavaScript: primeiros passos com a linguagem

---
+++ 5-atribuicaoDevariaveis.js
@@ -0,0 +1,13 @@
+console.log("trabalhando com atribuição de variaveis");
+
+const idade = 29;
+const nome = "Ricardo";
+const sobrenome = "Bugan";
+
+//console.log(nome +" " +sobrenome);
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome+sobrenome;
+console.log(nomeCompleto);
+
